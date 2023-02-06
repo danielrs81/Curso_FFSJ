e9ee2f683e3f2ff8180e3789103d64b0eb8bd6ad
Daniel Rodrigues da Silva <115389640+danielrs81@users.noreply.github.com>
2023-02-06 14:54:31 -0300

    up

---
+++ 7-Javascript-Avançado/aula10/script.js
@@ -0,0 +1,23 @@
+/*
+
+
+Síncrono VS Assíncrono
+
+
+*/
+
+
+
+function primeira() {
+    console.log('primeira')
+}
+
+function segunda() {
+    console.log('segunda')
+}
+
+
+setTimeout(primeira, 2000)   // assincrono
+
+segunda()
+
--- 7-Javascript-Avançado/aula9/script.js
+++ 7-Javascript-Avançado/aula9/script.js
@@ -1,24 +1,12 @@
 /*
 
-LOCAL STORAGE
+
+AJAX
+
+Asynchronous JavaScript And XML
+
 
 */
 
-const tarefas = [
-    { tarefa: 'estudar JS'},
-    { tarefa: 'estudar Node.Js'},
-    { tarefa: 'estudar react.js'},
-]
 
-const tarefasJson = JSON.stringify(tarefas)
-
-console.log(tarefasJson)
-
-localStorage.setItem('tarefas', tarefasJson)
-
-const listaTarefasSalvas = localStorage.getItem('tarefas')
-
-const listaTarefasObj = JSON.parse(listaTarefasSalvas)
-
-console.log(listaTarefasObj)
 
